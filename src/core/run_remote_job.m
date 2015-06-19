b40aa4888402cc1536ac9e1989afa29815f407f3
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-06-19 14:28:23 +0100

    added hyena support to run_remote_job - now the matlab worker quits if there is an error. this happens automagically for condor, but not for hyena

--- src/core/run_remote_job.m
+++ src/core/run_remote_job.m
@@ -1,21 +1,29 @@
 function run_remote_job(experiment_setup_data_path)
 !hostname
 task_dir = get_parent_dir(experiment_setup_data_path);
+diaries_folder=path_join(task_dir,'diaries');
+my_mkdir(diaries_folder);
+diary_file=create_increment_file( 'diary_',diaries_folder, 'txt', 1 );
+print_var(diary_file);
+diary(diary_file)
+
 try_folder = [task_dir filesep 'trys'];
 my_mkdir(try_folder)
-create_increment_file( 'condor', try_folder, 'try', 1 );
+create_increment_file( 'condor', try_folder, 'try', 0 );
 
 global job_root_dir
 load(experiment_setup_data_path,'worker_task','worker_args');
 job_root_dir = task_dir;
 
-worker_result = worker_task(worker_args{:});
-
-job_completion_file = fopen([task_dir filesep 'job_complete.txt'],'a');
-fclose(job_completion_file);
-
-myClock = clock;h = myClock(4);m = myClock(5);
-completion_time = strcat(num2str(h),'h',num2str(m),'m');
-save(experiment_setup_data_path, 'completion_time', '-append');
-save(experiment_setup_data_path, 'worker_result', '-append');
-
+try
+    worker_result = worker_task(worker_args{:});
+    job_completion_file = fopen([task_dir filesep 'job_complete.txt'],'a');
+    fclose(job_completion_file);
+    myClock = clock;h = myClock(4);m = myClock(5);
+    completion_time = strcat(num2str(h),'h',num2str(m),'m');
+    save(experiment_setup_data_path, 'completion_time', '-append');
+    save(experiment_setup_data_path, 'worker_result', '-append');
+catch job_err
+    fprintf(2, '%s\n', getReport(job_err, 'extended'));
+    exit;
+end
