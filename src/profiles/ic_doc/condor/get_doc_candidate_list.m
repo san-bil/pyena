beb955cdb826ce91669612fe4e101eed554306a2
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-07-14 11:22:09 +0100

    updated src/profiles/ic_doc/condor/get_doc_candidate_list.m so usernames are prepended to the candidate list

--- src/profiles/ic_doc/condor/get_doc_candidate_list.m
+++ src/profiles/ic_doc/condor/get_doc_candidate_list.m
@@ -1,4 +1,4 @@
-function all_candidates = get_doc_candidate_list(host_family_order)
+function all_candidates = get_doc_candidate_list(host_family_order, username)
 
 if(~exist('host_family_order','var'))
     host_family_order = {};
@@ -9,7 +9,8 @@ machine_count_map = kv_create_w_names('pixel',1:44,...
                                       'line',1:34,...
                                       'ray',1:40,...
                                       'corona',1:41,...
-                                      'edge',1:28 ...
+                                      'edge',1:28, ...
+                                      'ianos',1:6 ...
                                   );
 machine_count_map = kv_order_keys(host_family_order,machine_count_map);
 
@@ -18,9 +19,9 @@ keys = kv_getkeys(machine_count_map);
 for i = 1:length(keys)
     
     key = keys{i};
-    hostcount = kv_get(key,machine_count_map);
+    host_idxs = kv_get(key,machine_count_map);
     
-    tmp_hosts=cellfun_uo0(@(tmp) [key sprintf('%2.2d',tmp) '.doc.ic.ac.uk'], my_mat2cell(hostcount));
+    tmp_hosts=cellfun_uo0(@(tmp) [username '@' key sprintf('%2.2d',tmp) '.doc.ic.ac.uk'], my_mat2cell(host_idxs));
     all_candidates = [all_candidates tmp_hosts];
 
 end
