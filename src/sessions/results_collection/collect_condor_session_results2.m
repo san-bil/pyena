2d6beb520dc767b99eccde4ef824313146eea408
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-07-14 11:21:00 +0100

    updated functions to use the job_to_hosts_map to find which host a job is running on, and check that host for existence of the job_completion_file

--- src/sessions/results_collection/collect_condor_session_results2.m
+++ src/sessions/results_collection/collect_condor_session_results2.m
@@ -1,45 +1,45 @@
-function acc = collect_condor_session_results2(session_object, return_val_name, ignore_unfinished_jobs)
+function retval_acc = collect_condor_session_results2(session_object, return_val_name, ignore_unfinished_jobs,ssh_key)
 
-if(~exist('ignore_unfinished_jobs','var'))
-    ignore_unfinished_jobs = 0;
-end
-
+if(~exist('ignore_unfinished_jobs','var')); ignore_unfinished_jobs = 0; end
+if(~exist('ssh_key','var')),ssh_key=default_ssh_key;end;
 
 condor_task_cache_path = path_join(kv_get('condor_task_root_dir',session_object), 'session.mat');
 if(exist(condor_task_cache_path,'file'))
     load(condor_task_cache_path,'session_object');
 end
 
+condor_task_root_dir = kv_get('condor_task_root_dir', session_object);
 job_list = get_condor_session_job_list(session_object);
+job_to_hosts_map = kv_get('job_to_hosts_map', session_object);
+matador_pickup_remote_results = kv_get_recursive({'session_options','matador_pickup_remote_results'},session_object,0);
 
-acc = cell(length(job_list),1);
+retval_acc = cell(length(job_list),1);
 
 for i = 1:length(job_list)
     indicator_file = job_list{i};
+    job_host=kv_get(indicator_file,job_to_hosts_map);
+    is_job_complete = remote_file_exists(indicator_file,job_host,ssh_key);
     
-    if(~exist(indicator_file,'file'))
-        if(ignore_unfinished_jobs)
-            is_job_complete = 0;
-        else
-            user_input = input(['Job: ' indicator_file ' is not complete. Ignore? (y/n)']);
-            if(strcmp(user_input,'y'))
-                is_job_complete=0;
-            elseif(strcmp(user_input,'n'))
+    if(~is_job_complete)
+        if(~ignore_unfinished_jobs)
+            user_prompt_loop(['Job: ' indicator_file ' is not complete. Ignore? (y/n): '], {@(tmp)ismember(tmp,{'y','n'}),'You must type ''y'' or ''n''.'})
+            if(strcmp(user_input,'n'))
                 error(['Job: ' indicator_file ' is not complete.']);
-            else
-                error('You must type ''y'' or ''n''.')
             end
         end
     else
-        is_job_complete=1;
-    end
-    
-    if(is_job_complete==1)
-        task_dir = get_parent_dir(indicator_file);
-        task_mat = path_join(task_dir,'task_data.mat');
+        task_dir = dirname(indicator_file);
+        
+        if(~islocalhost(job_host) && matador_pickup_remote_results)
+           rsync(task_dir, condor_task_root_dir, rsync_args,job_host,default_ssh_key,'pull')
+           task_mat = path_join(condor_task_root_dir, basename(task_dir),'task_data.mat');
+        else
+           task_mat = path_join(task_dir,'task_data.mat');
+        end
+        
         load(task_mat,'worker_result');
         return_val = kv_get(return_val_name,worker_result);
-        acc{i} = return_val;
+        retval_acc{i} = return_val;
     end
     
 
--- src/sessions/results_collection/collect_condor_worker_result.m
+++ src/sessions/results_collection/collect_condor_worker_result.m
@@ -1,36 +1,45 @@
-function acc = collect_condor_worker_result(job_list, ignore_unfinished_jobs)
+function retval_acc = collect_condor_worker_result(job_list, ignore_unfinished_jobs, ssh_key)
 
-if(~exist('ignore_unfinished_jobs','var'))
-    ignore_unfinished_jobs = 0;
+if(~exist('ignore_unfinished_jobs','var')); ignore_unfinished_jobs = 0; end
+if(~exist('ssh_key','var')),ssh_key=default_ssh_key;end;
+
+condor_task_cache_path = path_join(kv_get('condor_task_root_dir',session_object), 'session.mat');
+if(exist(condor_task_cache_path,'file'))
+    load(condor_task_cache_path,'session_object');
 end
 
-acc = cell(length(job_list),1);
+condor_task_root_dir = kv_get('condor_task_root_dir', session_object);
+job_to_hosts_map = kv_get('job_to_hosts_map', session_object);
+matador_pickup_remote_results = kv_get_recursive({'session_options','matador_pickup_remote_results'},session_object,0);
+
+retval_acc = cell(length(job_list),1);
 
 for i = 1:length(job_list)
+    
     indicator_file = job_list{i};
+    job_host=kv_get(indicator_file,job_to_hosts_map);
+    is_job_complete = remote_file_exists(indicator_file,job_host,ssh_key);
     
-    if(~exist(indicator_file,'file'))
-        if(ignore_unfinished_jobs)
-            is_job_complete = 0;
-        else
-            user_input = input(['Job: ' indicator_file ' is not complete. Ignore? (y/n)']);
-            if(strcmp(user_input,'y'))
-                is_job_complete=0;
-            elseif(strcmp(user_input,'n'))
+    if(~is_job_complete)
+        if(~ignore_unfinished_jobs)
+            user_prompt_loop(['Job: ' indicator_file ' is not complete. Ignore? (y/n): '], {@(tmp)ismember(tmp,{'y','n'}),'You must type ''y'' or ''n''.'})
+            if(strcmp(user_input,'n'))
                 error(['Job: ' indicator_file ' is not complete.']);
-            else
-                error('You must type ''y'' or ''n''.')
             end
-        end
+        end;
     else
-        is_job_complete=1;
-    end
-    
-    if(is_job_complete==1)
-        task_dir = get_parent_dir(indicator_file);
-        task_mat = path_join(task_dir,'task_data.mat');
+        task_dir = dirname(indicator_file);
+        
+        if(~islocalhost(job_host) && matador_pickup_remote_results)
+           rsync(task_dir, condor_task_root_dir, rsync_args,job_host,default_ssh_key,'pull')
+           task_mat = path_join(condor_task_root_dir, basename(task_dir),'task_data.mat');
+        else
+           task_mat = path_join(task_dir,'task_data.mat');
+        end
+
+        
         load(task_mat,'worker_result');
-        acc{i} = worker_result;
+        retval_acc{i} = worker_result;
     end
     
 
