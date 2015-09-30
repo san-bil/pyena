b6aed3422d5f8fd148353913910d220c0d39bb8e
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-09-29 22:32:25 -0400

    added fn to simulate a pool of remote matador jobs but on the local box, for testing. that way we can create the per-job results folder.

---
+++ src/sessions/job_submission/submit_to_local_session.m
@@ -0,0 +1,23 @@
+
+function [worker_result] = submit_to_local_session(session_object,  worker_task, worker_args,  options)
+
+if(~exist('options','var')),options = {};end
+
+condor_task_cache_path = path_join(kv_get('condor_task_root_dir',session_object), 'session.mat');
+if(exist(condor_task_cache_path,'file'))
+    load(condor_task_cache_path,'session_object');
+end
+    
+
+condor_task_root_dir = kv_get('condor_task_root_dir',session_object);
+
+
+task_dir = path_join(condor_task_root_dir,kv_get('job_name',options,['local_' datestr(now,'dd-mm-yyyy-HH_MM_SS_FFF')])); 
+global job_root_dir
+job_root_dir = task_dir;
+
+worker_result=worker_task(worker_args{:});
+
+
+
+
