d804a57cd995124cc6eee4a07f72239527ee3e03
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-10-12 12:45:33 -0400

    bug fixes to get_unix_host_logged_in_users.m

--- src/hyena/pool_management/get_unix_host_logged_in_users.m
+++ src/hyena/pool_management/get_unix_host_logged_in_users.m
@@ -1,14 +1,20 @@
-function [out,is_host_up] = get_unix_host_logged_in_users(host_url)
+function [out,num_users,is_host_up] = get_unix_host_logged_in_users(host_url)
 
-string_args = {'ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 ',host_url,'"who | grep -oP ''^[^ ]+''  ;echo ENDSSH"'};
-cmd =  [ld_lib_path_fix build_string_args(string_args)];
-[~,stdout] =system(cmd);
-stdout_lines = filter_empty_strings( strsplit(stdout,'\n') );
-out = filter_string_list(stdout_lines,'ENDSSH',1);
-out = filter_string_list(out,'failed to connect to server',1);
-out = filter_string_list(out,'Warning: Permanently added',1);
-is_host_up = is_ssh_host_responding(out);
+is_host_up = is_ssh_host_responding2(host_url);
 
-if(~is_host_up)
+if(is_host_up)
+
+
+    string_args = {'ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 ',host_url,'"who | grep -oP ''^[^ ]+''  ;echo ENDSSH"'};
+    cmd =  [ld_lib_path_fix build_string_args(string_args)];
+    [~,stdout] =system(cmd);
+    stdout_lines = filter_empty_strings( strsplit(stdout,'\n') );
+    out = filter_string_list(stdout_lines,'ENDSSH',1);
+    out = filter_string_list(out,'failed to connect to server',1);
+    out = filter_string_list(out,'Warning: Permanently added',1);
+    num_users = length(out);
+    
+else
     fprintf('%s is unresponsive. \n', host_url)
+    num_users = Inf;
 end
--- src/hyena/pool_management/get_unix_host_tmux_sessions.m
+++ src/hyena/pool_management/get_unix_host_tmux_sessions.m
@@ -1,16 +1,21 @@
 function [out,is_host_up,num_sessions] = get_unix_host_tmux_sessions(host_url)
 
-string_args = {'ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 ',host_url,'"tmux list-sessions  ;echo ENDSSH"'};
-cmd =  [ld_lib_path_fix build_string_args(string_args)];
-[~,stdout] =system(cmd);
-stdout_lines = filter_empty_strings( strsplit(stdout,'\n') );
-out = filter_string_list(stdout_lines,'ENDSSH',1);
-out = filter_string_list(out,'failed to connect to server',1);
-out = filter_string_list(out,'Warning: Permanently added',1);
+is_host_up = is_ssh_host_responding2(host_url);
 
-is_host_up = is_ssh_host_responding(out);
-num_sessions = length(out);
+if(is_host_up)
+    
+    string_args = {'ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 ',host_url,'"tmux list-sessions  ;echo ENDSSH"'};
+    cmd =  [ld_lib_path_fix build_string_args(string_args)];
+    [~,stdout] =system(cmd);
+    stdout_lines = filter_empty_strings( strsplit(stdout,'\n') );
+    out = filter_string_list(stdout_lines,'ENDSSH',1);
+    out = filter_string_list(out,'failed to connect to server',1);
+    out = filter_string_list(out,'Warning: Permanently added',1);
 
-if(~is_host_up)
+    out = filter_string_list(out,'windows (created',0);
+
+    num_sessions = length(out);    
+else
     fprintf('%s is unresponsive. \n', host_url)
+    num_sessions = Inf;
 end
