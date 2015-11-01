1972d67adf087df639ade5c41fd2aa70db4d17c9
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-10-31 22:32:15 -0400

    fixed loop caching frequency bug for nested loop caches

--- src/nested_loop_caching/write_nested_loop_cache.m
+++ src/nested_loop_caching/write_nested_loop_cache.m
@@ -1,4 +1,4 @@
-if(use_loopcache && min(mod(loop_tracker,loopcache_frequency)==0)==0)
+if(use_loopcache && min(mod(loop_tracker+1,loopcache_frequency))==0)
   disp([get_simple_date() ': Caching ' concat_cell_string_array(kv_getkeys(loopcache_stateful),',',1) ' to ' loopcache_file])
   save(loopcache_file,'loopcache_stateful','loop_tracker','-v7.3');
   do_loopcacheload=0;
