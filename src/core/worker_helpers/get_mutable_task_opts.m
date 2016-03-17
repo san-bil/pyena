69dd26cb428b416e94e76b0a0be436598b0aeb16
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2016-03-17 01:36:28 +0000

    added function to pick up job opts that might change *DURING JOB EXECUTION* from a specified file in the task folder

---
+++ src/core/worker_helpers/get_mutable_task_opts.m
@@ -0,0 +1,7 @@
+function out = get_mutable_task_opts(key,default,handler)
+
+if(~exist('handler','var'))
+    handler=@my_str_to_num;
+end
+
+out = handler(get_mutable_opts(key,ggjrd('mutable_task_opts.ini'),default));
